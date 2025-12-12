bc53d2da9179d2c5c3483ed8fe369d2cb9113bc7
Maximillian Yang <Maxy2002@icloud.com>
2025-12-11 21:47:01 -0500

    Created HomeContorller class

---
+++ max-experimental-website/src/main/java/com/mx/max_experimental_website/HomeController.java
@@ -0,0 +1,13 @@
+package com.mx.max_experimental_website;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Max Experimentals!";
+    }
+}
