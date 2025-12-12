a8648c6f068b7e93c7237e342f5ea7090de9b0fc
Maximillian Yang <Maxy2002@icloud.com>
2025-12-11 23:36:06 -0500

    added thymeleaf dependency, changed spring Version from 4 to 3

--- max-experimental-website/src/main/java/com/mx/max_experimental_website/HomeController.java
+++ max-experimental-website/src/main/java/com/mx/max_experimental_website/HomeController.java
@@ -1,13 +1,14 @@
 package com.mx.max_experimental_website;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Max!";
+        System.out.println("HomeController called!");
+        return "index"; // must match templates/index.html
     }
 }
---
+++ max-experimental-website/src/test/java/com/mx/max_experimental_website/MaxExperimentalWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.max_experimental_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxExperimentalWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
